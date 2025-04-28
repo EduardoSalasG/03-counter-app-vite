29b4bbba85cd9385d52bf8a8b34a7bd70a3fc1d0
Eduardo Salas Gonzalez <89312228+EduardoSalasG@users.noreply.github.com>
2025-04-28 17:45:47 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import {HelloWorldApp} from './HelloWorldApp'
+import {FirstApp} from './FirstApp'
+import {CounterApp} from './CounterApp/CounterApp'
+
+import './styles.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+    <CounterApp value={4}/>
+        {/* <HelloWorldApp></HelloWorldApp> */}
+    </StrictMode>
+)
+
